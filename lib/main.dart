a57fd548269f03a9a9ebfab9ff3ed14e0a545a49
rezuanali <rasel15-5234@diu.edu.bd>
2025-02-19 10:31:01 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const lab05());
 
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class lab05 extends StatelessWidget {
+  const lab05({super.key});
 
   @override
   Widget build(BuildContext context) {
-  
+    return MaterialApp(
+      title : "This is new app",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fluter UI 2"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Container(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 60,
+              )
+
+               Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 60,
+              )
+
+              Image(image: )
+
+               Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 60,
+              )
+            ],
+          ),
+        )
+        
+      
+        ),
+      ),
+
+    )
   }
